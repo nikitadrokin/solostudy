2b8d626fa1896283ef129e7252a52d42b2cbb6d6
Nikita D <dnawastaken@gmail.com>
2025-08-13 16:04:54 -0700

    new project

--- apps/server/src/lib/auth.ts
+++ apps/server/src/lib/auth.ts
@@ -1,25 +1,23 @@
-
-import { betterAuth } from "better-auth";
-import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { db } from "../db";
-import * as schema from "../db/schema/auth";
+import { betterAuth } from 'better-auth';
+import { drizzleAdapter } from 'better-auth/adapters/drizzle';
+import { db } from '../db';
+import { account, session, user, verification } from '../db/schema/auth';
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
-    provider: "pg",
-    
-    
-    schema: schema,
+    provider: 'pg',
+
+    schema: {
+      user,
+      session,
+      account,
+      verification,
+    },
   }),
-  trustedOrigins: [
-    process.env.CORS_ORIGIN || "",
-  ],
+  trustedOrigins: [process.env.CORS_ORIGIN || ''],
   emailAndPassword: {
     enabled: true,
   },
   secret: process.env.BETTER_AUTH_SECRET,
   baseURL: process.env.BETTER_AUTH_URL,
 });
-
-
-
