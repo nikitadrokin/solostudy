fefd014786c410d979810071d4f6ff46c3625aba
Nikita D <dnawastaken@gmail.com>
2025-11-09 23:52:06 -0800

    unsloppified popover component

--- apps/web/src/components/ui/popover.tsx
+++ apps/web/src/components/ui/popover.tsx
@@ -22,31 +22,16 @@ function PopoverContent({
 }: React.ComponentProps<typeof PopoverPrimitive.Content>) {
   return (
     <PopoverPrimitive.Portal>
-      <div
+      <PopoverPrimitive.Content
+        align={align}
         className={cn(
-          'relative flex max-h-[500px] flex-col pr-4 pl-2',
-          'before:absolute before:inset-x-0 before:top-0 before:z-10 before:h-[var(--gradient-height-top)] before:rounded-t-3xl before:bg-gradient-to-b before:from-[var(--gradient-color)] before:to-transparent before:content-[""]',
-          'after:pointer-events-none after:absolute after:inset-x-0 after:bottom-0 after:z-10 after:h-[var(--gradient-height-bottom)] after:bg-gradient-to-t after:from-[var(--gradient-color)] after:to-transparent after:content-[""]'
+          'data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 w-72 origin-(--radix-popover-content-transform-origin) rounded-2xl border bg-popover p-4 text-popover-foreground shadow-md outline-hidden data-[state=closed]:animate-out data-[state=open]:animate-in',
+          className
         )}
-        style={
-          {
-            '--gradient-height-top': '3.5rem',
-            '--gradient-height-bottom': '3.5rem',
-            '--gradient-color': 'var(--background)',
-          } as React.CSSProperties
-        }
-      >
-        <PopoverPrimitive.Content
-          align={align}
-          className={cn(
-            'data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 w-72 origin-(--radix-popover-content-transform-origin) rounded-md border bg-popover p-4 text-popover-foreground shadow-md outline-hidden data-[state=closed]:animate-out data-[state=open]:animate-in',
-            className
-          )}
-          data-slot="popover-content"
-          sideOffset={sideOffset}
-          {...props}
-        />
-      </div>
+        data-slot="popover-content"
+        sideOffset={sideOffset}
+        {...props}
+      />
     </PopoverPrimitive.Portal>
   );
 }
