e0ed980b94f47b2c651308cd610e2367ef9de99c
Nikita D <dnawastaken@gmail.com>
2025-11-20 00:05:18 -0800

    added ghost-destructive variant

--- apps/web/src/components/ui/button.tsx
+++ apps/web/src/components/ui/button.tsx
@@ -20,6 +20,7 @@ const buttonVariants = cva(
           'bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80',
         ghost:
           'hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50',
+        'ghost-destructive': 'hover:!text-white hover:bg-destructive/90',
         link: 'text-primary underline-offset-4 hover:underline',
       },
       size: {
