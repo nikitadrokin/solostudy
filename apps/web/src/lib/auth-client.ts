7b7ad39c84d4fd87734fb29e93562feb55e86c2b
Nikita D <dnawastaken@gmail.com>
2025-11-23 21:58:09 -0800

    better auth passkeys?

--- apps/web/src/lib/auth-client.ts
+++ apps/web/src/lib/auth-client.ts
@@ -1,12 +1,10 @@
-import { apiKeyClient, passkeyClient } from 'better-auth/client/plugins';
+import { passkeyClient } from '@better-auth/passkey/client';
+import { apiKeyClient } from 'better-auth/client/plugins';
 import { createAuthClient } from 'better-auth/react';
 
 export const authClient = createAuthClient({
   baseURL: process.env.NEXT_PUBLIC_SERVER_URL,
-  plugins: [
-    apiKeyClient(),
-    passkeyClient(),
-  ],
+  plugins: [apiKeyClient(), passkeyClient()],
 });
 
 export const { signIn, signOut, signUp, useSession } = authClient;
