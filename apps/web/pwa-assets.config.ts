8a87d772fc94600af0e2e44cf82785f4a1b27597
Nikita D <dnawastaken@gmail.com>
2025-12-15 20:18:50 -0800

    reverted pwa preset

--- apps/web/pwa-assets.config.ts
+++ apps/web/pwa-assets.config.ts
@@ -1,15 +1,6 @@
-import {
-  defineConfig,
-  minimal2023Preset,
-  combinePresetAndAppleSplashScreens,
-} from '@vite-pwa/assets-generator/config';
+import { defineConfig } from '@vite-pwa/assets-generator/config';
 
 export default defineConfig({
-  headLinkOptions: {
-    preset: '2023',
-  },
-  preset: combinePresetAndAppleSplashScreens(minimal2023Preset, {
-    darkResizeOptions: { background: '#000000', fit: 'contain' },
-  }),
+  preset: 'minimal',
   images: ['src/app/favicon.ico'],
 });
