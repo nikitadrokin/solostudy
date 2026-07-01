9f270e788833f29114ff89cf1c73005cd069f327
Nikita D <dnawastaken@gmail.com>
2026-07-01 16:12:45 -0700

    applying migrations to sqlite db

--- apps/web/src/db/index.ts
+++ apps/web/src/db/index.ts
@@ -1,3 +1,10 @@
+import Database from 'better-sqlite3';
 import { drizzle } from 'drizzle-orm/better-sqlite3';
+import { migrate } from 'drizzle-orm/better-sqlite3/migrator';
 
-export const db = drizzle(process.env.DATABASE_URL || '');
+const client = new Database(
+  process.env.DATABASE_URL || '/app/data/database.db'
+);
+export const db = drizzle(client);
+
+migrate(db, { migrationsFolder: './src/db/migrations' });
