3228b7bfafc7d279ae0f1ae7000f288b1372eb13
Nikita D <dnawastaken@gmail.com>
2026-07-01 15:15:24 -0700

    switching to sqlite

--- apps/web/drizzle.config.ts
+++ apps/web/drizzle.config.ts
@@ -3,8 +3,8 @@ import { defineConfig } from 'drizzle-kit';
 export default defineConfig({
   schema: './src/db/schema',
   out: './src/db/migrations',
-  dialect: 'postgresql',
+  dialect: 'sqlite',
   dbCredentials: {
-    url: process.env.DATABASE_URL || '',
+    url: process.env.DATABASE_URL || '/app/data/database.db',
   },
 });
